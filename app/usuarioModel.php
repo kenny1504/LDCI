a5e8e4ac700ff3716d883f4acfb74adb731b7b6c
Guissell17 <aldraespinoza@yahoo.es>
2020-10-18 21:47:56 -0600

    Actualizacion de consultas

--- app/usuarioModel.php
+++ app/usuarioModel.php
@@ -13,7 +13,7 @@ class usuarioModel extends Model
     public function GetUsuario($password,$user)
     {
         $query = new static;
-        $query= DB::select("select * from ldci.tb_usuarios where pass=? and usuario=?",[$password,$user]);
+        $query= DB::select("select * from ldci.tb_usuario where password=? and usuario=?",[$password,$user]);
         return $query;
     }
 
@@ -21,7 +21,7 @@ class usuarioModel extends Model
     public function GetDatosUsuario($id_usuario)
     {
         $query = new static;
-        $query= DB::select("select usuario,telefono,pass from ldci.tb_usuarios where id_usuario=?;",[$id_usuario]);
+        $query= DB::select("select usuario,telefono,password from ldci.tb_usuario where id_usuario=?;",[$id_usuario]);
         return $query;
     }
 
@@ -29,8 +29,8 @@ class usuarioModel extends Model
     public function registrarUsuario($password,$user,$correo,$telefono,$codigo_confirmacion)
     {
         $query = new static;
-        $query= DB::select("INSERT INTO ldci.tb_usuarios(
-            usuario, tipo, pass,fecha_grabacion,  usuario_grabacion, telefono,codigo_confirmacion, correo)
+        $query= DB::select("INSERT INTO ldci.tb_usuario(
+            usuario, tipo, password,fecha_grabacion,  usuario_grabacion, telefono,codigo_confirmacion, correo)
            VALUES ( ?, ?, ?, now(), ?, ?, ?, ?)",[$user,3,$password,0,$telefono,$codigo_confirmacion,$correo]);
         return $query;
     }
@@ -39,7 +39,7 @@ class usuarioModel extends Model
     public function verificarCorreo($codigo_confirmacion)
     {
         $query = new static;
-        $query= DB::select("select * from ldci.tb_usuarios where codigo_confirmacion=?",[$codigo_confirmacion]);
+        $query= DB::select("select * from ldci.tb_usuario where codigo_confirmacion=?",[$codigo_confirmacion]);
         return $query;
     }
 
@@ -47,8 +47,8 @@ class usuarioModel extends Model
      public function actualizarEstado($id_usuario)
      {
          $query = new static;
-         $query= DB::select("UPDATE ldci.tb_usuarios
-                            SET  fecha_modificacion=now(), usuario_modifica=0,  confirmado=true, codigo_confirmacion=null
+         $query= DB::select("UPDATE ldci.tb_usuario
+                            SET  fecha_modificacion=now(), usuario_modificacion=0,  confirmado=true, codigo_confirmacion=null
                             WHERE id_usuario=?",[$id_usuario]);
          return $query;
      }
