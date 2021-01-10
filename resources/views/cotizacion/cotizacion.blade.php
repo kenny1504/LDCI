50a7f5f33bb35099d75c7996f21abc6fc6c71e10
kenny saenz <kennysaenz31@gmail.com>
2021-01-10 12:20:09 -0600

    Mejorando botones de tablas Dinamicas

--- resources/views/cotizacion/cotizacion.blade.php
+++ resources/views/cotizacion/cotizacion.blade.php
@@ -151,7 +151,7 @@
                                 </td>
 
                                 <td width="10%" class="text-center">
-                                    <div class="pull-right row form-group">
+                                    <div class=" form-group">
                                         <button class="btn btn-danger eliminarFila"
                                                 title="Eliminar servicio"
                                                 id="btnEliminarFila">
