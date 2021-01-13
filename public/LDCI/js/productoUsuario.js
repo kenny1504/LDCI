a235ee23288bb0c07ec57b0a6df113fc1d0c95a8
kenny saenz <kennysaenz31@gmail.com>
2021-01-13 00:29:53 -0600

    Animacion, Vista productos

--- public/LDCI/js/productoUsuario.js
+++ public/LDCI/js/productoUsuario.js
@@ -12,89 +12,100 @@ $(document).ready(function () {
         },
         success: function (data) {
 
-            data.forEach(element => {
+            if (Object.entries(data).length==0)
+            {
 
-                var id_producto=element.id_producto
-                var precio=element.precio
-                /** Recupera todas las imagenes de un producto */
-                $.ajax({
-                    type: 'POST',
-                    url: '/producto/getProductoImagenes', //llamada a la ruta
-                    data: {
-                        _token:$('input[name=_token]').val(),
-                        id_producto:id_producto
-                    },
-                    success: function (data) {
+                  var animacion="<div class=\"bb\"></div>"
 
-                        var carousel="#listbox"+id_producto;
-                        var carou="<figure   class=\"figure tag tag-sale figure"+id_producto+"\">"+
-                            "</figure>" +
-                            "<div id=\"carousel"+id_producto+"\" class=\"carousel slide\" data-ride=\"carousel\">\n" +
-                            "    <ol class=\"carousel-indicators\">\n" +
-                            "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"0\" class=\"active\"></li>\n" +
-                            "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"1\"></li>\n" +
-                            "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"2\"></li>\n" +
-                            "    </ol>\n" +
-                            "    <div id=\"listbox"+id_producto+"\" class=\"carousel-inner\">\n" +
-                            "    </div>\n" +
-                            "    <a class=\"carousel-control-prev\" href=\"#carousel"+id_producto+"\" role=\"button\" data-slide=\"prev\">\n" +
-                            "        <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>\n" +
-                            "        <span class=\"sr-only\">Previous</span>\n" +
-                            "    </a>\n" +
-                            "    <a class=\"carousel-control-next\" href=\"#carousel"+id_producto+"\" role=\"button\" data-slide=\"next\">\n" +
-                            "        <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>\n" +
-                            "        <span class=\"sr-only\">Next</span>\n" +
-                            "    </a>\n" +
-                            "</div>"
+                $('#principal').append(animacion);
+                showLoad();
+                alertError("Nos quedamos sin productos :O")
+            }
+                else
+            {
+                data.forEach(element => {
 
-                        /** Añadiendo precio a etiqueta*/
-                        carou+="<style>\n" +
-                            "    .figure.tag-sale.figure"+id_producto+"::before{\n" +
-                            "        content: \"$"+precio+"\";\n" +
-                            "        background: #00d95a;\n" +
-                            "    }\n" +
-                            "</style>"
+                    var id_producto=element.id_producto
+                    var precio=element.precio
+                    /** Recupera todas las imagenes de un producto */
+                    $.ajax({
+                        type: 'POST',
+                        url: '/producto/getProductoImagenes', //llamada a la ruta
+                        data: {
+                            _token:$('input[name=_token]').val(),
+                            id_producto:id_producto
+                        },
+                        success: function (data) {
 
-                        $('#principal').append(carou);
+                            var carousel="#listbox"+id_producto;
+                            var carou="<figure   class=\"figure tag tag-sale figure"+id_producto+"\">"+
+                                "</figure>" +
+                                "<div id=\"carousel"+id_producto+"\" class=\"carousel slide\" data-ride=\"carousel\">\n" +
+                                "    <ol class=\"carousel-indicators\">\n" +
+                                "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"0\" class=\"active\"></li>\n" +
+                                "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"1\"></li>\n" +
+                                "        <li data-target=\"#carousel"+id_producto+"\" data-slide-to=\"2\"></li>\n" +
+                                "    </ol>\n" +
+                                "    <div id=\"listbox"+id_producto+"\" class=\"carousel-inner\">\n" +
+                                "    </div>\n" +
+                                "    <a class=\"carousel-control-prev\" href=\"#carousel"+id_producto+"\" role=\"button\" data-slide=\"prev\">\n" +
+                                "        <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>\n" +
+                                "        <span class=\"sr-only\">Previous</span>\n" +
+                                "    </a>\n" +
+                                "    <a class=\"carousel-control-next\" href=\"#carousel"+id_producto+"\" role=\"button\" data-slide=\"next\">\n" +
+                                "        <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>\n" +
+                                "        <span class=\"sr-only\">Next</span>\n" +
+                                "    </a>\n" +
+                                "</div>"
 
-                        var html='',control=1;
-                        /** recorre arreglo (Rutas de imagenes de un producto) */
-                        data.forEach(element => {
+                            /** Añadiendo precio a etiqueta*/
+                            carou+="<style>\n" +
+                                "    .figure.tag-sale.figure"+id_producto+"::before{\n" +
+                                "        content: \"$"+precio+"\";\n" +
+                                "        background: #00d95a;\n" +
+                                "    }\n" +
+                                "</style>"
 
-                            if(control==1)
-                            {
-                                html+="<div class=\"carousel-item active\">\n" +
-                                    "            <img style='width: 70% !important' class=\"d-block w-100\" src="+element.url+"/"+element.imagen+" alt=\"First slide\">\n" +
-                                    "<div class=\"carousel-caption d-none d-md-block\">\n" +
-                                    "    <h5>"+element.nombre+"</h5>\n" +
-                                    "<p>"+element.descripcion+"</p>"+
-                                    "  </div>" +
-                                    " </div>"
-                            }else
-                            {
-                                html+="<div class=\"carousel-item\">\n" +
-                                    "            <img style='width: 70% !important' class=\"d-block w-100\" src="+element.url+"/"+element.imagen+" alt=\"First slide\">\n" +
-                                    "<div class=\"carousel-caption d-none d-md-block\">\n" +
-                                    "    <h5>"+element.nombre+"</h5>\n" +
-                                    "<p>"+element.descripcion+"</p>"+
-                                    "  </div>" +
-                                    " </div>"
-                            }
-                            control++;
+                            $('#principal').append(carou);
 
-                        });
+                            var html='',control=1;
+                            /** recorre arreglo (Rutas de imagenes de un producto) */
+                            data.forEach(element => {
 
-                        $(carousel).append(html);
-                        showLoad(false);
+                                if(control==1)
+                                {
+                                    html+="<div class=\"carousel-item active\">\n" +
+                                        "            <img style='width: 70% !important' class=\"d-block w-100\" src="+element.url+"/"+element.imagen+" alt=\"First slide\">\n" +
+                                        "<div class=\"carousel-caption d-none d-md-block\">\n" +
+                                        "    <h5>"+element.nombre+"</h5>\n" +
+                                        "<p>"+element.descripcion+"</p>"+
+                                        "  </div>" +
+                                        " </div>"
+                                }else
+                                {
+                                    html+="<div class=\"carousel-item\">\n" +
+                                        "            <img style='width: 70% !important' class=\"d-block w-100\" src="+element.url+"/"+element.imagen+" alt=\"First slide\">\n" +
+                                        "<div class=\"carousel-caption d-none d-md-block\">\n" +
+                                        "    <h5>"+element.nombre+"</h5>\n" +
+                                        "<p>"+element.descripcion+"</p>"+
+                                        "  </div>" +
+                                        " </div>"
+                                }
+                                control++;
 
-                    },
-                    error: function (err) {
-                        alertError(err.responseText);
-                        showLoad(false);
-                    }
-                });
-            })
+                            });
 
+                            $(carousel).append(html);
+                            showLoad(false);
+
+                        },
+                        error: function (err) {
+                            alertError(err.responseText);
+                            showLoad(false);
+                        }
+                    });
+                })
+            }
         },
         error: function (err) {
             alertError(err.responseText);
--- resources/views/catalogos/productoUsuario.blade.php
+++ resources/views/catalogos/productoUsuario.blade.php
@@ -52,9 +52,71 @@
     }
 
 
+    .bb::before, .bb::after, .bb {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        content: "Nos Quedamos Sin Productos :O";
+    }
+
+    .bb {
+        width: 200px;
+        height: 200px;
+        margin: auto;
+        background: url("//ldci.cargologisticsintermodal.com/LDCI/img/Logo-Intermodal.png") no-repeat 50%/70% rgba(0, 0, 0, 0.1);
+        color: #4444dd;
+        box-shadow: white;
+    }
+    .bb::before, .bb::after {
+        content: "";
+        z-index: -1;
+        margin: -5%;
+        box-shadow: inset 0 0 0 2px;
+        animation: clipMe 8s linear infinite;
+    }
+    .bb::before {
+        animation-delay: -4s;
+    }
+    .bb:hover::after, .bb:hover::before {
+        background-color: lightskyblue;
+    }
+
+    @keyframes clipMe {
+        0%, 100% {
+            clip: rect(0px, 220px, 2px, 0px);
+        }
+        25% {
+            clip: rect(0px, 2px, 220px, 0px);
+        }
+        50% {
+            clip: rect(218px, 220px, 220px, 0px);
+        }
+        75% {
+            clip: rect(0px, 220px, 220px, 218px);
+        }
+    }
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        position: relative;
+        background-color: white;
+    }
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
 </style>
 
 <script src="{{asset("LDCI/js/productoUsuario.js")}}" ></script>
+
 
 
 
