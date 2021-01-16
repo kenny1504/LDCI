504a977bba7344573be88e0790e8d5561fb7ed01
kenny saenz <kennysaenz31@gmail.com>
2021-01-15 23:22:06 -0600

    Reporte factura venta

--- public/LDCI/js/verFactura.js
+++ public/LDCI/js/verFactura.js
@@ -163,3 +163,33 @@ var tblFacturas = null;
 
     }
 
+/** Funcion que genera reporte */
+function rpt_factura_productos(datos)
+{
+    var dt = tblFacturas.row($(datos).parents('tr')).data();
+    var  codigoFactura= dt[0];
+
+    showLoad(true);
+    var _token= $('input[name=_token]').val();
+
+    $.ajax({
+        type:"post",
+        url: '/productos/factura', //llamada a la ruta
+        global:false,
+        data:{
+            _token:_token,
+            codigoFactura:codigoFactura
+        }
+    })
+        .done(function(data,textstatus,jqXHR )
+        {
+            var nombrelogico="pdf"
+            var parametros="dependent=yes,locationbar=no,scrollbars=yes,menubar=yes,resizable,screenX=80,screenY=80,width=900,height=1400";
+            var htmltext="<embed width=100% height=100% type='application/pdf' src='data:application/pdf,"+escape(data) +"'></enbed>";
+            var detailwindows= window.open("",nombrelogico,parametros);
+            detailwindows.document.write(htmltext);
+            detailwindows.document.close();
+            showLoad(false);
+        });
+}
+
